63dc85c3cb71b1e200128928c455afe845cae5bc
David Rodriguez <santiago1.dsrr@gmail.com>
2025-02-28 23:21:31 -0500

    event listener to go to individual period pages

--- controllers/index.js
+++ controllers/index.js
@@ -18,6 +18,36 @@ router.get(`/`, async (req, res) => {
     };    
 });
 
+//route to single period page
+router.get(`/period-page/:id`, async (req, res) => {
+    try{
+        //get period data
+        const period = await Period.findByPk(req.params.id, {
+            include: [Development, Source]
+        });
+        //render single period page
+        res.render(`period-page`, {period});
+        console.log(`Single period page rendered ;)`);
+    } catch(err){
+        console.log(`Something went wrong rendering Single Period Page :(`, err);
+        res.status(500).json({msg: `Something went wrong`, err});
+    };
+});
+
+//route to sources page
+router.get(`/sources-page`, async (req, res) => {
+    try{
+        //get source data
+        const sources = await Source.findAll();
+        //render sources page
+        res.render(`sources-page`, {sources});
+        console.log(`Sources page rendered ;)`);
+    } catch(err){
+        console.log(`Something went wrong rendering Sources Page :(`, err);
+        res.status(500).json({msg: `Something went wrong`, err});
+    };
+});
+
 //period routes
 const periodRoutes = require(`./periodRoutes`);
 router.use(`/periods`, periodRoutes);
